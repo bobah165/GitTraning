d774b7957aad8b5724731b830998fdd273b18c3d
vkalenov <vekalynov@gmail.com>
2020-01-14 17:57:20 +0300

    commit branch

--- src/main/java/com/Hello.java
+++ src/main/java/com/Hello.java
@@ -2,6 +2,7 @@ package com;
 
 public class Hello {
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello this branch");
+        System.out.println();
     }
 }
